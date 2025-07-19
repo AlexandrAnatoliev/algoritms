c073816a17686bf062f6df3ac97b5c3a507a02ab
samsung <per-1986@list.ru>
2025-07-19 12:49:32 +0000

    feat: program accept number row, randomize its and print numbers

--- p2/randomize-array/src/RandomizeArray.java
+++ p2/randomize-array/src/RandomizeArray.java
@@ -2,20 +2,30 @@
  * RandomizeArray
  *
  * @author AlexandrAnatoliev
- * @version 0.1.3 2025-07-17
+ * @version 0.2.0 2025-07-19
  */
 public class RandomizeArray {
   public static void main(String[] args) {
 
     int[] array = new int[args.length];
+
     if (args.length > 0) {
       for(int i = 0; i < args.length; i++) {
         array[i] = Integer.parseInt(args[i]);
       }
     }
-    for (int i = 0; i < array.length; i++) {
-      System.out.println(array[i]);
+
+    for (int i = 0; i < array.length - 1; i++) {
+      int randomIndex = getNextNumber(i, i + 1, args.length);
+      int temp = array[randomIndex];
+      array[randomIndex] = array[i];
+      array[i] = temp;
     }
+
+    for (int i = 0; i < array.length; i++) {
+      System.out.print(array[i] + " " );
+    }
+    System.out.print("\n");
   }
 
   /**
@@ -24,8 +34,8 @@ public class RandomizeArray {
    * @param number Natural number
    * @return Random number
    */
-  public static int getNextNumber(int number) { 
-    return (7 * number + 5) % 11;
+  public static int getNextNumber(int number, int min, int max) { 
+    return min + (7 * number + 5) % max - min;
   }
 
 }
