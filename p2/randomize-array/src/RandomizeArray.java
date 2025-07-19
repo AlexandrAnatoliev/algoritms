1e0f92e65a99a7d2da85f4d4b83967282987cf43
samsung <per-1986@list.ru>
2025-07-19 11:02:22 +0000

    fix: add test for check input

--- p2/randomize-array/src/RandomizeArray.java
+++ p2/randomize-array/src/RandomizeArray.java
@@ -8,6 +8,9 @@ public class RandomizeArray {
   public static void main(String[] args) {
 
     int [] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    if (args.length > 0) {
+      System.out.println("yes");
+    }
     for (int i = 0; i < array.length; i++) {
       System.out.println(array[i]);
     }
