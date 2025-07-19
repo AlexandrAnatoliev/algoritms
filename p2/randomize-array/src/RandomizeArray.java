b53837558d71680de59d92c6e13b18650881ea18
samsung <per-1986@list.ru>
2025-07-19 10:55:50 +0000

    fix: use array.length method

--- p2/randomize-array/src/RandomizeArray.java
+++ p2/randomize-array/src/RandomizeArray.java
@@ -2,14 +2,13 @@
  * RandomizeArray
  *
  * @author AlexandrAnatoliev
- * @version 0.1.0 2025-07-17
+ * @version 0.1.1 2025-07-17
  */
 public class RandomizeArray {
   public static void main(String[] args) {
 
     int [] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int lenght = 10;
-    for (int i = 0; i < lenght; i++) {
+    for (int i = 0; i < array.length; i++) {
       System.out.println(array[i]);
     }
   }
