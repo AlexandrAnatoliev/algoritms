b843b29db603288b1391c296ee4a6ac248f578f2
samsung <per-1986@list.ru>
2025-07-19 16:59:56 +0000

    feat: program receive 2 numbers and print its gcd

--- p2/gcd/src/GCD.java
+++ p2/gcd/src/GCD.java
@@ -2,23 +2,29 @@
  * RandomizeArray
  *
  * @author AlexandrAnatoliev
- * @version 0.1.0 2025-07-19
+ * @version 0.2.0 2025-07-19
  */
 public class GCD {
   public static void main(String[] args) {
     int num1 = Integer.parseInt(args[0]);
     int num2 = Integer.parseInt(args[1]);
 
-    System.out.println(num1 + " " + num2);
+    System.out.println("GCD " + num1 + " and " + num2 + " = " + getGCD(num1, num2));
   }
 
   /**
-   * Method to get next random number
+   * Method to get great common divisor
    *
-   * @param number Natural number
-   * @return Random number
+   * @param number1 First number
+   * @param number2 Second number
+   * @return GCD
    */
-  public static int getNextNumber(int number, int min, int max) { 
-    return (int)(min + (double)((7 * number + 5) % max) / (max) * (max - min));
+  public static int getGCD(int number1, int number2) { 
+    while(number2 != 0) {
+      int remainder = number1 % number2;
+      number1 = number2;
+      number2 = remainder;
+    }
+    return number1;
   }
 }
